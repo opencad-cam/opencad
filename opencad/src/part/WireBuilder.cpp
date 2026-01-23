38ec308b465e3b614c52cf487053a3710c8b44fe
OpenCAD <opencads@gmail.com>
2026-01-23 22:51:34 +0300

    Revert "Fix Extrude selection and double geometry bug"

--- opencad/src/part/WireBuilder.cpp
+++ opencad/src/part/WireBuilder.cpp
@@ -19,23 +19,18 @@
 #include <BRepClass_FaceClassifier.hxx>
 #include <BRepGProp.hxx>
 #include <BRep_Builder.hxx>
-#include <BRep_Tool.hxx>
 #include <GC_MakeArcOfCircle.hxx>
 #include <GC_MakeCircle.hxx>
 #include <GC_MakeSegment.hxx>
 #include <GProp_GProps.hxx>
 #include <Geom_TrimmedCurve.hxx>
 #include <ShapeAnalysis_Wire.hxx>
-#include <TopExp_Explorer.hxx>
 #include <TopoDS.hxx>
 #include <TopoDS_Compound.hxx>
-#include <TopoDS_Vertex.hxx>
-#include <algorithm>
 #include <gp_Ax2.hxx>
 #include <gp_Circ.hxx>
 #include <gp_Dir.hxx>
 #include <gp_Pnt.hxx>
-#include <vector>
 
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
@@ -242,80 +237,24 @@ TopoDS_Shape WireBuilder::buildFaces(const std::vector<TopoDS_Wire> &wires) {
   if (wires.empty())
     return TopoDS_Shape();
 
-  // 1. Create planar face for each wire to check areas and containment
-  struct WireInfo {
-    TopoDS_Wire wire;
-    TopoDS_Face tempFace;
-    double area;
-    bool used;
-  };
-
-  std::vector<WireInfo> wireInfos;
-  for (const auto &wire : wires) {
-    if (wire.IsNull())
-      continue;
-
-    BRepBuilderAPI_MakeFace mkFace(wire, true);
-    // true = only planar
-    if (mkFace.IsDone()) {
-      TopoDS_Face face = mkFace.Face();
-      GProp_GProps props;
-      BRepGProp::SurfaceProperties(face, props);
-      wireInfos.push_back({wire, face, props.Mass(), false});
-    }
-  }
-
-  // 2. Sort by Area Descending (Largest first)
-  std::sort(
-      wireInfos.begin(), wireInfos.end(),
-      [](const WireInfo &a, const WireInfo &b) { return a.area > b.area; });
-
+  // Build a face for each wire independently (no hole detection)
   TopoDS_Compound resultCompound;
   BRep_Builder builder;
   builder.MakeCompound(resultCompound);
   bool addedAny = false;
 
-  // 3. Process wires: Largest are Outers, smaller inside them are Holes
-  for (size_t i = 0; i < wireInfos.size(); ++i) {
-    if (wireInfos[i].used)
+  for (const auto &wire : wires) {
+    if (wire.IsNull())
       continue;
 
-    // Current largest is Outer
-    WireInfo &outer = wireInfos[i];
-    outer.used = true;
-
-    BRepBuilderAPI_MakeFace faceBuilder(outer.wire, true);
-
-    // Check remaining wires to see if they are holes inside this outer
-    for (size_t j = i + 1; j < wireInfos.size(); ++j) {
-      if (wireInfos[j].used)
-        continue;
-
-      WireInfo &potentialHole = wireInfos[j];
-
-      // Check if hole is inside outer
-      // Use a point from hole's wire/face
-      // Get center or a vertex? BRepClass_FaceClassifier checks a point.
-      // Let's use a point on the wire of the hole.
-
-      // Or easier: Vertex of hole
-      TopExp_Explorer exp(potentialHole.wire, TopAbs_VERTEX);
-      if (exp.More()) {
-        TopoDS_Vertex v = TopoDS::Vertex(exp.Current());
-        gp_Pnt p = BRep_Tool::Pnt(v);
-
-        BRepClass_FaceClassifier classifier(outer.tempFace, p, 1e-6);
-        if (classifier.State() == TopAbs_IN) {
-          // It is a hole!
-          faceBuilder.Add(potentialHole.wire);
-          potentialHole.used = true;
-        }
+    try {
+      BRepBuilderAPI_MakeFace faceMaker(wire, true);
+      if (faceMaker.IsDone()) {
+        builder.Add(resultCompound, faceMaker.Face());
+        addedAny = true;
       }
-    }
-
-    if (faceBuilder.IsDone()) {
-      builder.Add(resultCompound, faceBuilder.Face());
-      addedAny = true;
+    } catch (...) {
+      continue;
     }
   }
 
